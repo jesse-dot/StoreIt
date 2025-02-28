cc28776b9221bb84608b0aece2f5aae0cdf52150
jesse-dot <67440987+jesse-dot@users.noreply.github.com>
2025-02-27 19:41:40 -0500

    Update file.actions.ts

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -213,7 +213,7 @@ export async function getTotalSpaceUsed() {
       audio: { size: 0, latestDate: "" },
       other: { size: 0, latestDate: "" },
       used: 0,
-      all: 2 * 1024 * 1024 * 1024 /* 2GB available bucket storage */,
+      all: 50 * 1024 * 1024 * 1024 /* 50GB available bucket storage */,
     };
 
     files.documents.forEach((file) => {
